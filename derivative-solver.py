c3806d81785943b177a5f36592635cf3c5fe2c72
rtenacity <50297222+rtenacity@users.noreply.github.com>
2020-07-31 18:31:35 -0400

    added an derivative formula solver

---
+++ derivative-solver.py
@@ -0,0 +1,7 @@
+import cmath
+print("Derivative solver.")
+a = int(input("constant = "))
+b = input("variable = ")
+c = float(input("power = "))
+
+print("The formula is " + str(a*c) + str(b) + "^" + str(c-1))
