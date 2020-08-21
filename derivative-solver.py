6ce525491b53d95eb0f8828df19f60f3965f2354
rtenacity <50297222+rtenacity@users.noreply.github.com>
2020-08-21 17:59:11 -0400

    added some more stuff

--- derivative-solver.py
+++ derivative-solver.py
@@ -5,3 +5,17 @@ b = input("variable = ")
 c = float(input("power = "))
 
 print("The formula is " + str(a*c) + str(b) + "^" + str(c-1))
+
+d = c - 1.0
+
+e = a*c
+
+f = float(input("x = "))
+
+g = f**d
+
+h = g*e
+
+print(str(b)+ " = " + str(h))
+
+
